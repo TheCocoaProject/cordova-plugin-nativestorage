af87b1165c0827410942d2b5e79c0180a72b48cc
alokrajiv <mail@alokrajiv.com>
2016-05-01 16:48:02 +0400

    Added a test for object

--- tests/main.js
+++ tests/main.js
@@ -1,7 +1,24 @@
 exports.defineAutoTests = function () {
     
     describe('Write/Read Tests', function () {
-        
+        it('Objects', function () {
+            var dummyData = { data1: "", data2: 2, data3: 3.0 };
+            NativeStorage.set("ref_obj",
+                dummyData,
+                function (result) {
+                    NativeStorage.getObject("ref_obj",
+                        function (result) {
+                            expect(result).toEqual(dummyData);
+                        },
+                        function (e) {
+                            fail("Read Object Failed");
+                        });
+                },
+                function (e) {
+                    fail("Write Object Failed");
+                });
+
+        });
     });
 
 };
