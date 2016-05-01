aec398fb0b63fd02b5dc4e4e13738034cdb379e2
alokrajiv <mail@alokrajiv.com>
2016-05-01 17:09:11 +0400

    Fixed async wait on object test

--- tests/main.js
+++ tests/main.js
@@ -1,14 +1,15 @@
 exports.defineAutoTests = function () {
     
     describe('Write/Read Tests', function () {
-        it('Objects', function () {
+        it('Objects', function (done) {
             var dummyData = { data1: "", data2: 2, data3: 3.0 };
-            NativeStorage.set("ref_obj",
+            NativeStorage.set("dummy_ref_obj",
                 dummyData,
                 function (result) {
-                    NativeStorage.getObject("ref_obj",
+                    NativeStorage.getObject("dummy_ref_obj",
                         function (result) {
                             expect(result).toEqual(dummyData);
+                            done();
                         },
                         function (e) {
                             fail("Read Object Failed");
